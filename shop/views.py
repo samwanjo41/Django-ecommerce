5108b9b94448c624a0c1ed461dc7689eb6b40926
Samuel Wanjohi <swanjohi@tracom.co.ke>
2020-07-17 15:51:25 +0300

    Initial commit

---
+++ shop/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def store(request):
+    context = {}
+    return render(request, 'shop/store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'shop/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'shop/checkout.html', context)
